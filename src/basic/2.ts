cd93037f7555b5a3bb20fef78cfd4bb5c3c0a12e
Yurii <evseevyrij90@gmail.com>
2025-05-18 16:10:10 +0200

    basis done

---
+++ src/basic/2.ts
@@ -0,0 +1,5 @@
+const person: [string, number] = ["Max", 21];
+
+console.log("друге завдання!");
+
+console.log(person);
---
+++ src/basic/3.ts
@@ -0,0 +1,12 @@
+let unionValue: string | number;
+
+let literalValue: "enable" | "disable";
+
+unionValue = "Привіт";
+unionValue = 123;
+
+literalValue = "enable";
+
+console.log(unionValue);
+
+console.log(literalValue);
